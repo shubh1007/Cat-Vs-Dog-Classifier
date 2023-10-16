4ff9a1a553bc78e02fd2e5720413dbeeae51deb1
shubh1007 <b200045@nitsikkim.ac.in>
2023-10-16 20:21:37 +0530

    well documented code for the model

--- functions.py
+++ functions.py
@@ -1,16 +1,22 @@
+# Library used for web-application
 import streamlit as st
+# Library used for working with image
 from PIL import Image
+# Used for Loading the pre-trained MobileNetV2 model and making predictions
 import tensorflow as tf
-import numpy
 
 # Load the pre-trained MobileNetV2 model
 model = tf.keras.applications.MobileNetV2(weights='imagenet')
 
 # Function to preprocess and classify the image
 def classify_image(image):
+    # Resizes the image into 224 x 224 pixels
     image = tf.image.resize(image, (224, 224))
+    # Preprocessing of the image and preparing it for classification.
     image = tf.keras.applications.mobilenet_v2.preprocess_input(image)
     predictions = model.predict(tf.expand_dims(image, axis=0))
+    # uses loaded model to make predictions on the processed image
+    # It decodes the predictions into a list of tuples (class, description, probability)
     decoded_predictions = tf.keras.applications.mobilenet_v2.decode_predictions(predictions)
     return decoded_predictions[0][0][1]
 
